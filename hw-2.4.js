d0787c6ada2c6a3b6fb651339e652cb93548ee67
AliftinaMashtak <al.zvonkovitch@yandex.ru>
2023-03-05 23:38:53 +0300

    changes hw-2.4

---
+++ hw-2.4.js
@@ -0,0 +1,47 @@
+// Задание 1
+
+// let i = 0;
+
+// while (i < 2) {
+//     console.log("Привет");
+//     i++;
+// }
+
+// Задание 2
+
+// let i = 1;
+
+// do {
+//   console.log(i);
+// 	i++;
+// } while (i <= 5);
+
+// Задание 3
+
+// for (let i = 7; i <= 22; i++) {
+//     console.log(i);
+//   }
+
+// Задание 4
+
+// const obj = {
+//     'Коля': '200',
+//     'Вася': '300',
+//     'Петя': '400'
+// };
+// for (let salary in obj){
+//    console.log(`${salary} — зарплата ${obj[salary]} долларов`);
+// }
+
+// Задание 5
+
+// let num = 0;
+// for (let n = 1000; n > 50; n /= 2, num ++) {
+//     console.log (num,n);
+// }
+
+// Задание 6
+
+// for (let friday = 5; friday <= 31; friday += 7) {
+//     alert (`Сегодня пятница ${friday} -е число. Необходимо подготовить отчет`);
+// }
